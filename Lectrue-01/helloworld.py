9f555db191603748143b28a175d85f696dad7802
naf603599-stack <naf603599@gmail.com>
2026-06-30 14:58:58 +0700

    t

---
+++ Lectrue-01/helloworld.py
@@ -0,0 +1,10 @@
+print("Hello world")
+print("Information And Network Engineerig")
+print("Siriyaporn You Will Be Great")
+
+print("I'm Hungry")
+print('He ask me "Who got the books?"')
+
+print("I'm good and \"I will do my best\"")
+
+print("/\\")
