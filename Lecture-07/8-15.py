a23f197e3aa3990792e1321d12529a997cd1adeb
naf603599-stack <naf603599@gmail.com>
2026-09-01 16:11:44 +0700

    07

--- Lecture-07/8-15.py
+++ Lecture-07/8-15.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import struct
 with open("records.bin","rb")as file:
     record_size = struct.calcsize('i20sif')
@@ -7,4 +8,15 @@ with open("records.bin","rb")as file:
             break
         record = struct.unpack('i20sif',data)
         record = (record[0],record[1].decode().strip('\x00'),record[2],record[3])
+=======
+import struct
+with open("records.bin","rb")as file:
+    record_size = struct.calcsize('i20sif')
+    while True:
+        data = file.read(record_size)
+        if not data:
+            break
+        record = struct.unpack('i20sif',data)
+        record = (record[0],record[1].decode().strip('\x00'),record[2],record[3])
+>>>>>>> 0c8a140320000645ebb57b05ba11c74ce7305e4c
         print(f"ID: {record[0]},Name: {record[1]},Age: {record[2]},GPA: {record[3]}")
--- Lecture-07/8-17.py
+++ Lecture-07/8-17.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import struct
 record_format='i20sif'
 record_size = struct.calcsize(record_format)
@@ -6,4 +7,14 @@ with open("records.bin","rb")as file:
     data = file.read(record_size)
     record = struct.unpack(record_format,data)
     record = (record[0],record[1].decode().strip('\x00'),record[2],record[3])
+=======
+import struct
+record_format='i20sif'
+record_size = struct.calcsize(record_format)
+with open("records.bin","rb")as file:
+    file.seek(record_size)   
+    data = file.read(record_size)
+    record = struct.unpack(record_format,data)
+    record = (record[0],record[1].decode().strip('\x00'),record[2],record[3])
+>>>>>>> 0c8a140320000645ebb57b05ba11c74ce7305e4c
     print(f"ID: {record[0]},Name: {record[1]},Age: {record[2]},GPA: {record[3]}")
